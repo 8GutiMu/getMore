a8f6a23cf4d8f5d903e3e15508ddab2eb90f8748
8GutiMu <you@example.com>
2017-09-05 15:00:33 -0500

    maquetacion header

---
+++ js/blickUtils.js
@@ -0,0 +1,22 @@
+var showAlert = function (event) {
+	event.preventDefault()
+	
+	$.getJSON("http://demo6292426.mockable.io/blickTest", function (response) {
+		
+		showData(response)
+	})
+}
+
+var showData = function(data){
+	console.log(data)
+	var alert_ = data.name
+	alert_ += " "+data.email
+	
+	alert(alert_)
+}
+
+
+var btnContacto = document.getElementById("contacto-btn")
+console.log(btnContacto)
+
+btnContacto.addEventListener("click", showAlert)
