c0a2c55f5792a9da70d611721b5808ad8f3fcf1c
8GutiMu <you@example.com>
2017-09-05 13:28:08 -0500

    funcion de caracteres

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
+var abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var cont = 0;
 
-var hasUniqueCharacters = function(value){
-	console.log(value)
+var checkBoolean = function(){
+	
 	
 }
 
-hasUniqueCharacters("HOLA")
-hasUniqueCharacters("HOLA COMO ESTAS?")
+var hasUniqueCharacters = function (values) {
+
+	values = values.split("")
+	console.log(values)
+
+	abecedario.forEach(function(letra){
+		values.forEach(function(value){
+			if (letra == value) {
+				cont += 1;
+			} 
+			
+			
+		})
+	})
+	
+	if(values.length == cont){
+		console.log("TRUE")
+	}else{
+		console.log("FALSE")
+	}
+	
+
+}
+hasUniqueCharacters("hola")
+hasUniqueCharacters("hola HOLAcomo estas")
+hasUniqueCharacters("holacomoestas?")
+
